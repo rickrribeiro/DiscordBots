0644f763f1ad5b7b5d7ff0875c30c83340bb138e
ricrdobah <rickribeiro99@gmail.com>
2020-06-03 01:01:34 -0300

    list and new commands

--- app.js
+++ app.js
@@ -9,26 +9,38 @@ client.on("ready", ()=>{
 })
 
 client.on("message", msg =>{
-    if(msg.content.toLowerCase().includes("melhor druida")){
-        var gerado = Math.floor((Math.random() * 10) + 1)
-        if(gerado == 1 || gerado == 2 || gerado == 3){
-            msg.reply("Você ainda tem duvidas que não é o Grande Rickbahia!?!?!?")
-        }else  if(gerado == 4 || gerado == 5 || gerado == 6){
-            msg.reply("É obvio que é o Rickbahia!!")
-        }else  if(gerado == 7 || gerado == 8 || gerado == 9){
-            msg.reply("Trouer?Mastersan?Magalzera? Sai dai! O Rickbahia é o Deus!")
-        }else if(gerado == 10){
-            msg.reply("Rickbahia é o rei dos druidas!")
+    if(msg.content.toLowerCase().includes("rick,")){
+        if(msg.content.toLowerCase().includes("melhor druida")){
+            var gerado = Math.floor((Math.random() * 10) + 1)
+            if(gerado == 1 || gerado == 2 || gerado == 3){
+                msg.reply("Você ainda tem duvidas que não é o Grande Rickbahia!?!?!?")
+            }else  if(gerado == 4 || gerado == 5 || gerado == 6){
+                msg.reply("É obvio que é o Rickbahia!!")
+            }else  if(gerado == 7 || gerado == 8 || gerado == 9){
+                msg.reply("Trouer?Mastersan?Magalzera? Sai dai! O Rickbahia é o Deus!")
+            }else if(gerado == 10){
+                msg.reply("Rickbahia é o rei dos druidas!")
+            }
+            
         }
-        
+    
+
+
     }
-  
 
     if(msg.content.toLowerCase() == "rick?"){
-        msg.reply("Eai!\n1.Pergunta ai quem é o top druida do svr.\n")
+        msg.reply(`Eai noobs! Para eu não ficar floodando o chat, antes de cada mensagem coloque "Rick," e faça sua pergunta!
+                    1.Melhor druida
+                    2.DKP
+                    3.Builds
+                    4.Addons
+                    5.Horarios
+                    6.Avisos
+                    7.Nudes`)
     }
 
+   
   // console.log(msg.content.toLowerCase())
 })
 //end funcionalidades
-client.login("")
+client.login("quer oq aqui otario?")

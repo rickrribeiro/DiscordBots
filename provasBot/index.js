220e46151773af944d4b69c640e4306897673d46
rickrribeiro <rickribeiro99@gmail.com>
2021-10-05 21:59:05 -0300

    adicionar questoes e provas

--- provasBot/index.js
+++ provasBot/index.js
@@ -1,6 +1,8 @@
 var Discord = require("discord.js");
 const client = new Discord.Client();
 const db = require("./provas.json")
+const env = require("./env.json")
+const fs = require("fs")
 //funcionalidades
 client.on("ready", ()=>{
     
@@ -21,21 +23,81 @@ client.on("message", async msg =>{
     }
     else if(msg.content.toLowerCase() == "?prova lista"){
         let str='';
-        let i;
+  
         db.exams.forEach((el, i)=>{
             str+=`\n${i+1}. ${el.name}`
         })
         msg.reply(str)
     }else if(msg.content.toLowerCase().includes("?prova adicionar")){
-        let name = msg.content.toLowerCase().split("?prova adicionar")[1]
-        if(name){
-            db.exams.push({name:name})
-            console.log(db)
-            msg.reply("Prova adicionada")
-        }else{
-            msg.reply("Utilize o comando no formato \"prova adicionar {nome da prova}\"")
+        try{
+
+            let name = msg.content.toLowerCase().split("?prova adicionar ")[1]
+            if(name){
+                db.exams.push({name:name,questions:[]})
+                fs.writeFile('./provas.json', JSON.stringify(db), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("Salvou");
+                });
+                msg.reply("Prova adicionada")
+            }else{
+                msg.reply("Utilize o comando no formato \"prova adicionar {nome da prova}\"")
+            }
+        }catch(err){
+            console.log(err);
+            msg.reply("Deu erro! Fale com rickrribeiro p saber ql foi")
         }
-    }
-})
-//end funcionalidades
-client.login("")
+    }else if(msg.content.toLowerCase().includes("?prova questoes")){
+        try{
+            
+            let name = msg.content.toLowerCase().split("?prova questoes ")[1].split(" ")[0]
+            
+            
+            let exam;
+            if(name){
+                exam = db.exams.find(el => el.name==name)
+                
+                let str='';
+            }else{
+                msg.reply("Utilize o comando no formato \"prova questoes {nome da prova}\"")
+            }
+            if(exam && msg.content.toLowerCase().includes("adicionar")){
+                exam.questions.push({name: "aaaaaaaaaaaaaaaaa"})
+                fs.writeFile('./provas.json', JSON.stringify(db), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("Salvou");
+                });
+                msg.reply("Salvou!")
+            }else{
+                
+               if(exam){   
+                   let str=''
+                   exam.questions.forEach((el, i)=>{
+                       str+=`\n${i+1}. ${el.name}`
+                    })
+                    if(str!=''){
+                        msg.reply(str)
+                    }else{
+                        msg.reply("Nenhuma questão adicionada!")
+                    }
+                }else{
+                    msg.reply("Prova nao encontrada!")
+                }
+                    
+            }
+        }catch(err){
+            console.log(err)
+            msg.reply("Não entendi! Utilize o comando help para saber mais!")
+        }
+            
+        }
+        
+        
+        
+        
+    })
+    //end funcionalidades
+    client.login(env.token)
